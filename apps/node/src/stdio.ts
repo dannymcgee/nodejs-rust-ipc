224d9a6dd14b56517958fb6d47252f5ac4f40954
Danny McGee <dannymcgee@gmail.com>
2021-05-08 13:31:22 -0400

    Misc fixes

--- apps/node/src/stdio.ts
+++ apps/node/src/stdio.ts
@@ -1,8 +1,6 @@
 import Path from "path";
 import cp from "child_process";
 import chalk from "chalk";
-import { Socket } from "dgram";
-import { Server } from "http";
 
 const EXE_PATH = "apps/rust/target/debug/rust.exe";
 
@@ -10,11 +8,9 @@ export default async function() {
 	let exe = Path.resolve(process.cwd(), EXE_PATH);
 	let proc = cp.spawn(exe, { stdio: ["pipe", process.stdout, process.stderr] });
 
-	proc.on("message", onMessage);
 	proc.on("close", onClose);
 	proc.on("error", handleError);
 	proc.on("exit", onExit);
-	proc.on("disconnect", onDisconnect);
 
 	const tick = () => {
 		proc.stdin.write("Tick!\n", (err) => {
@@ -22,6 +18,7 @@ export default async function() {
 			else setTimeout(tick, 500);
 		});
 	}
+
 	tick();
 }
 
@@ -31,13 +28,9 @@ function handleError(err: Error) {
 	process.exit(1);
 }
 
-function onClose(code: number) {
+function onClose(code: number, sig: NodeJS.Signals) {
 	console.log("onClose");
-	process.exit(code);
-}
-
-function onDisconnect(code: number) {
-	console.log("onDisconnect");
+	if (sig) console.log("signal:", sig);
 	process.exit(code);
 }
 
@@ -45,9 +38,4 @@ function onExit(code: number, sig: NodeJS.Signals) {
 	console.log("onExit");
 	if (sig) console.log("signal:", sig);
 	process.exit(code);
-}
-
-function onMessage(msg: any, handle: Socket|Server) {
-	console.log("onMessage:", msg);
-	console.log("handle:", handle);
 }
